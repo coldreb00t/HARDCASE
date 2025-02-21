5a2065690302e29443caf876d69283e940383932
coldreb00t <eugene.a.melnik@gmail.com>
2025-02-21 18:24:41 +0300

    клиенты

--- src/components/TrainerDashboard.tsx
+++ src/components/TrainerDashboard.tsx
@@ -25,7 +25,7 @@ export function TrainerDashboard({ defaultView = 'calendar' }: TrainerDashboardP
   const menuItems = [
     {
       icon: <Users className="w-5 h-5 min-w-[20px]" />,
-      label: 'Клиентосы',
+      label: 'Клиенты',
       onClick: () => {
         setActiveView('clients');
         navigate('/trainer/clients');
